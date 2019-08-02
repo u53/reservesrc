7d91a6ca80997cc07b49c23af9a2bb332df43613
u53 <1065378079@qq.com>
2019-08-02 11:10:11 +0800

    预约服务器

---
+++ src/main/java/com/ly/reservesrc/ReservesrcApplication.java
@@ -0,0 +1,15 @@
+package com.ly.reservesrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ReservesrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservesrcApplication.class, args);
+    }
+
+}
