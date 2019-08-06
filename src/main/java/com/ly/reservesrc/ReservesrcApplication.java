930982fc9c5e24463484a7bf54616bec093f9355
u53 <1065378079@qq.com>
2019-08-06 23:48:05 +0800

    预约服务器

--- src/main/java/com/ly/reservesrc/ReservesrcApplication.java
+++ src/main/java/com/ly/reservesrc/ReservesrcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ReservesrcApplication {
+public class ReservesrcApplication   {
 
     public static void main(String[] args) {
         SpringApplication.run(ReservesrcApplication.class, args);
